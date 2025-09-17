4e4eaa1f7ca452aaad628e5d9e720456f832db68
ishansharma2 <is5290@pleasantonusd.net>
2025-09-17 01:07:21 +0000

    done3

--- robot/Driver.java
+++ robot/Driver.java
@@ -1,84 +1,101 @@
 package robot;
 
+
 import kareltherobot.*;
+
 
 public class Driver implements Directions {
 
-    private static Robot roomba;
-    private static int st = 0;
-    private static int av = 0;
 
-    public static void turnRight() {
-        roomba.turnLeft();
-        roomba.turnLeft();
-        roomba.turnLeft();
-    }
+   private static Robot roomba;
+   private static int st = 0;
+   private static int av = 0;
 
-    public static void main(String[] args) {
-        String wrldName = "robot/finalTestWorld2024.wld";
 
-        World.readWorld(wrldName);
-        World.setVisible(true);
+   public static void turnRight() {
+       roomba.turnLeft();
+       roomba.turnLeft();
+       roomba.turnLeft();
+   }
 
-        double largest_pile = 0;
-        double piles = 0;
-        double total_piles = 0;
-        double total = 0;
-        double area = 1;
 
-        roomba = new Robot(26, 149, West, 0);
-        World.setDelay(0);
+   public static void main(String[] args) {
+       String wrldName = "robot/finalTestWorld2024.wld";
 
-        boolean clear = roomba.frontIsClear();
 
-        while (clear == true) {
-            while (clear) {
-                boolean scan = roomba.nextToABeeper();
+       World.readWorld(wrldName);
+       World.setVisible(true);
 
-                if (scan == false && clear) {
-                    area++;
-                    roomba.move();
-                    scan = roomba.nextToABeeper();
-                }
 
-                int beepers = 0;
-                piles = 0;
+       double largest_pile = 0;
+       double piles = 0;
+       double total_piles = 0;
+       double total = 0;
+       double area = 1;
 
-                while (scan) {
-                    roomba.pickBeeper();
-                    total++;
-                    scan = roomba.nextToABeeper();
-                    beepers++;
-                    piles++;
 
-                    if (piles == 1) {
-                        total_piles += 1; // count this pile once
-                    }
-                    if (beepers > largest_pile) {
-                        largest_pile = beepers;
-                        st = roomba.street();
-                        av = roomba.avenue();
-                    }
-                }
+       roomba = new Robot(26, 149, West, 0);
+       World.setDelay(0);
 
-                clear = roomba.frontIsClear();
-            }
 
-            clear = roomba.frontIsClear();
-            boolean faceEast = roomba.facingEast();
-            boolean faceWest = roomba.facingWest();
+       boolean clear = roomba.frontIsClear();
 
-            if (clear == false) {
-                if (faceEast == true) {
-                    roomba.turnLeft();
-                    clear = roomba.frontIsClear();
-                    if (clear) {
-                        area++;
-                        roomba.move();
-                        roomba.turnLeft();
-                        clear = roomba.frontIsClear();
-                    } else {
-                        area++;
+
+       while (clear == true) {
+           while (clear) {
+               boolean scan = roomba.nextToABeeper();
+
+
+               if (scan == false && clear) {
+                   area++;
+                   roomba.move();
+                   scan = roomba.nextToABeeper();
+               }
+
+
+               int beepers = 0;
+               piles = 0;
+
+
+               while (scan) {
+                   roomba.pickBeeper();
+                   total++;
+                   scan = roomba.nextToABeeper();
+                   beepers++;
+                   piles++;
+
+
+                   if (piles == 1) {
+                       total_piles += 1; // count this pile once
+                   }
+                   if (beepers > largest_pile) {
+                       largest_pile = beepers;
+                       st = roomba.street();
+                       av = roomba.avenue();
+                   }
+               }
+
+
+               clear = roomba.frontIsClear();
+           }
+
+
+           clear = roomba.frontIsClear();
+           boolean faceEast = roomba.facingEast();
+           boolean faceWest = roomba.facingWest();
+
+
+           if (clear == false) {
+               if (faceEast == true) {
+                   roomba.turnLeft();
+                   clear = roomba.frontIsClear();
+                   if (clear) {
+                       area++;
+                       roomba.move();
+                       roomba.turnLeft();
+                       clear = roomba.frontIsClear();
+                   } else {
+                       area++;
                        roomba.turnOff();
                        System.out.println("Area of Room: " + area);
                        System.out.println("Total Amount of Piles: " + total_piles + " piles");
@@ -95,11 +112,6 @@ public class Driver implements Directions {
                        area++;
                        roomba.move();
                        turnRight();
-                      
-                        if (clear) {
-                       area++;
-                       roomba.move();
-                       turnRight();
                        clear = roomba.frontIsClear();
                    } else {
                        area++;
@@ -111,14 +123,12 @@ public class Driver implements Directions {
                        System.out.println("Location of Largest Pile: (" + st + ", " + av + ")");
                        System.out.println("Average pile size: " + (total / total_piles) + " beepers");
                        System.out.println("Percent room dirty: " + ((total_piles * 100) / area) + "%");
-
-                   } 
-                    
-
-                    }
-                }
-
-            }
-        }
-    }
+                   }
+               }
+           }
+       }
+   }
 }
+
+
+
