c714dcc4ec5ffc225fc8ddd66c6621a819ba2b86
ishansharma2 <is5290@pleasantonusd.net>
2025-09-17 00:43:06 +0000

    d

--- robot/Driver.java
+++ robot/Driver.java
@@ -19,8 +19,35 @@ public class Driver implements Directions {
 
         World.readWorld(wrldName);
         World.setVisible(true);
-        
 
-       
+        double largest_pile = 0;
+        double piles = 0;
+        double total_piles = 0;
+        double total = 0;
+        double area = 1;
+
+        roomba = new Robot(26, 149, West, 0);
+        World.setDelay(0);
+
+        boolean clear = roomba.frontIsClear();
+
+        while (clear == true) {
+            while (clear) {
+                boolean scan = roomba.nextToABeeper();
+
+                if (scan == false && clear) {
+                    area++;
+                    roomba.move();
+                    scan = roomba.nextToABeeper();
+                }
+
+                
+
+
+
+
+
+            }
+        }
     }
 }
