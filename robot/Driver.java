90a459b7947d0e0154b4ff4f286a1aa41446e344
ishansharma2 <is5290@pleasantonusd.net>
2025-09-17 00:26:54 +0000

    done

---
+++ robot/Driver.java
@@ -0,0 +1,25 @@
+package robot;
+
+import kareltherobot.*;
+
+public class Driver implements Directions {
+
+    private static Robot roomba;
+    private static int st = 0;
+    private static int av = 0;
+
+    public static void turnRight() {
+        roomba.turnLeft();
+        roomba.turnLeft();
+        roomba.turnLeft();
+    }
+
+    public static void main(String[] args) {
+        String wrldName = "robot/finalTestWorld2024.wld";
+
+        World.readWorld(wrldName);
+        World.setVisible(true);
+
+       
+    }
+}
