4c1fe2a6faff76ba5bd2738149b4fe870172ae32
ishansharma2 <is5290@pleasantonusd.net>
2025-12-13 20:31:02 +0000

    FINAL COMMIT and finished project

--- gol/GameOfLife.java
+++ gol/GameOfLife.java
@@ -1,14 +1,14 @@
+package gol;
+
 public class GameOfLife implements Board {
 
-    // Integers: 0 or 1 for alive or dead
     private int[][] board;
 
-    public GameOfLife(int x, int y) {
-        // Construct a 2d array of the given x and y size.
-        board = new int[x][y]; // defaults to all 0s
+    public GameOfLife(int x, int y)
+    {
+        board = new int[x][y];
     }
 
-    // Set values on the board
     public void set(int x, int y, int[][] data) {
         for (int i = 0; i < data.length; i++) {
             for (int j = 0; j < data[0].length; j++) {
@@ -17,78 +17,62 @@ public class GameOfLife implements Board {
         }
     }
 
-        // Set values on the board
-    public void set(int x, int y, int[][] data) {
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data[0].length; j++) {
-                board[i + x][j + y] = data[i][j];
-            }
-        }
-    }
-
-    // Run the simulation for a number of turns
     public void run(int turns) {
-        for (int t = 0; t < turns; t++) {
+        for (int i = 0; i < turns; i++) {
             step();
         }
     }
 
-}
-// Step the simulation forward one turn.
-    public void step() {
-        print();
+    public void step()
+    {
+        int[][] next = new int[board.length][board[0].length];
 
-        int rows = board.length;
-        int cols = board[0].length;
+        for (int x = 0; x < board.length; x++) {
+            for (int y = 0; y < board[0].length; y++) {
 
-        int[][] next = new int[rows][cols];
-
-        for (int x = 0; x < rows; x++) {
-            for (int y = 0; y < cols; y++) {
                 int neighbors = countNeighbors(x, y);
-                int current = get(x, y);
 
-                // Conway's rules:
-                // Alive cell survives if it has 2 or 3 neighbors
-                // Dead cell becomes alive if it has exactly 3 neighbors
-                if (current == 1) {
-                    next[x][y] = (neighbors == 2 || neighbors == 3) ? 1 : 0;
+                if (board[x][y] == 1) {
+                    if (neighbors == 2 || neighbors == 3) {
+                        next[x][y] = 1;
+                    } else {
+                        next[x][y] = 0;
+                    }
                 } else {
-                    next[x][y] = (neighbors == 3) ? 1 : 0;
+                    if (neighbors == 3) {
+                        next[x][y] = 1;
+                    }
                 }
             }
         }
+
         board = next;
     }
 
     public int countNeighbors(int x, int y) {
         int count = 0;
 
-        // Count all 8 neighbors using wraparound get()
         for (int dx = -1; dx <= 1; dx++) {
             for (int dy = -1; dy <= 1; dy++) {
                 if (dx == 0 && dy == 0) continue;
                 count += get(x + dx, y + dy);
             }
         }
-
         return count;
     }
-  // Get a value from the board with "wrap around"
+
     public int get(int x, int y) {
         int xLimit = board.length;
         int yLimit = board[0].length;
         return board[(x + xLimit) % xLimit][(y + yLimit) % yLimit];
     }
 
-    // Test helper to get the whole board state
-    public int[][] get() {
+    public int[][] get()
+    {
         return board;
     }
 
-    // Test helper to print the current state
-    public void print() {
-        // Print the header
+    public void print(){
         System.out.print("\n ");
         for (int y = 0; y < board[0].length; y++) {
             System.out.print(y % 10 + " ");
@@ -96,12 +80,12 @@ public class GameOfLife implements Board {
 
         for (int x = 0; x < board.length; x++) {
             System.out.print("\n" + x % 10);
-            for (int y = 0; y < board[x].length; y++) {
-                if (board[x][y] == 1) {
+            for (int y = 0; y < board[x].length; y++)
+            {
+                if (board[x][y] == 1)
                     System.out.print("⬛");
-                } else {
+                else
                     System.out.print("⬜");
-                }
             }
         }
         System.out.println();
--- gol/TestSuite.java
+++ gol/TestSuite.java
@@ -1,7 +1,5 @@
 package gol;
 
-import java.util.Arrays;
-
 public class TestSuite {
     public static void run()
     {
@@ -10,22 +8,17 @@ public class TestSuite {
 
         GameOfLife board = new GameOfLife(5, 5);
 
-        // Test count neighbors
         int[][] data = {{ 1 }};
 
-        // Set a single cell, then check that it has zero neighbors.
         board.set(2, 2, data);
         pass &= expect(board.countNeighbors(2,2), 0, "Single live cell with zero neighbors");
 
-        // Set the middle top neighbor.
         board.set(1, 2, data);
         pass &= expect(board.countNeighbors(2,2), 1, "Single live cell with one neighbor");
 
-        // Set the bottom middle neighbor.
         board.set(3, 2, data);
         pass &= expect(board.countNeighbors(2,2), 2, "Single live cell with two neighbors");
 
-        // Test update state
         board.step();
         board.print();
         pass &= expect(board.get(2,3), 1, "Expect line to rotate");
@@ -34,30 +27,16 @@ public class TestSuite {
         board.print();
         pass &= expect(board.get(1,2), 1, "Expect line to rotate");
 
-
-        if (pass == true)
-        {
+        if (pass)
             System.out.println("--- TEST PASSED! Congrats! ---");
-        }
         else
-        {
             System.out.println("--- TEST FAILED! :( ---");
-        }
     }
 
     private static boolean expect(int input, int expected, String comment)
     {
-        if (input == expected)
-        {
-            return true;
-        }
+        if (input == expected) return true;
         System.out.println(comment + ", value: " + input + " not equal to expected: " + expected);
         return false;
     }
-
-    private static boolean testPattern(int[][] input, int[][] expected)
-    {
-        return true;
-    }
-
 }
