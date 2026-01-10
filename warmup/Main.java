80643529d807e1af40fdad1483463aea6e9ea7b4
ishansharma2 <is5290@pleasantonusd.net>
2026-01-10 03:56:51 +0000

    2

--- warmup/Main.java
+++ warmup/Main.java
@@ -1,27 +1,25 @@
 package warmup;
 
 public class Main {
-
     public static void main(String[] args) {
+
         int limit = 4000000;
 
-        int first =1;
-        int second =2;
-
+        int first = 1;
+        int second = 2;
         int sum = 0;
 
-        while (second <= limit){
-            if(second % 2 ==0){
+        while (second <= limit) {
+
+            if (second % 2 == 0) {
                 sum = sum + second;
             }
+
             int next = first + second;
-    }
             first = second;
             second = next;
-        
-        }     
-             System.out.println(sum);
+        }
+
+        System.out.println(sum);
     }
-
-
 }
